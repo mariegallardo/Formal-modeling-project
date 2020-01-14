1312330994b718287605f2faa999a3611c8a39a6
marie.gallardo <marie.gallardo@efrei.net>
2020-01-14 10:16:51 +0100

    First version

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Clinic/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Clinic from './components/Clinic.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    //HelloWorld
+    Clinic
   }
 }
 </script>
---
+++ src/components/Clinic.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>This is the clinic component</h1>
+</template>
+
+<script>
+</script>
